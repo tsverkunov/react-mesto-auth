41f67a681730c43eb1f38789fce0aac101413567
ITsverkunov <tsverkunovivan@gmail.com>
2022-07-12 23:47:12 +0300

    fix: test CORS

--- src/utils/api.js
+++ src/utils/api.js
@@ -2,24 +2,25 @@ class Api {
   constructor({baseUrl, headers, credentials}) {
     this._baseUrl = baseUrl
     this._headers = headers
-    // this._credentials = credentials
+    this._credentials = credentials
   }
 
   _getResponseData(res) {
-    return !res.ok ? Promise.reject(`Ошибка тут: ${res.status}`) : res.json()
+    return !res.ok ? Promise.reject(`Ошибка: ${res.status}`) : res.json()
   }
 
   getProfile() {
     return fetch(`${this._baseUrl}/users/me`, {
       headers: this._headers,
-      // credentials: this._credentials,
+      credentials: this._credentials,
     })
       .then(res => this._getResponseData(res))
   }
 
   getInitialCards() {
     return fetch(`${this._baseUrl}/cards`, {
-      headers: this._headers
+      headers: this._headers,
+      credentials: this._credentials,
     })
       .then(res => this._getResponseData(res))
   }
@@ -28,6 +29,7 @@ class Api {
     return fetch(`${this._baseUrl}/cards/${id}/likes`, {
       method: !isLiked ? 'DELETE' : 'PUT',
       headers: this._headers,
+      credentials: this._credentials,
     })
       .then(res => this._getResponseData(res))
   }
@@ -39,7 +41,8 @@ class Api {
       body: JSON.stringify({
         name,
         about
-      })
+      }),
+      credentials: this._credentials,
     })
       .then(res => this._getResponseData(res))
   }
@@ -51,7 +54,8 @@ class Api {
       body: JSON.stringify({
         name,
         link
-      })
+      }),
+      credentials: this._credentials,
     })
       .then(res => this._getResponseData(res))
   }
@@ -60,6 +64,7 @@ class Api {
     return fetch(`${this._baseUrl}/cards/${id}`, {
       method: 'DELETE',
       headers: this._headers,
+      credentials: this._credentials,
     })
       .then(res => this._getResponseData(res))
   }
@@ -70,7 +75,8 @@ class Api {
       headers: this._headers,
       body: JSON.stringify({
         avatar
-      })
+      }),
+      credentials: this._credentials,
     })
       .then(res => this._getResponseData(res))
   }
@@ -82,5 +88,5 @@ export const api = new Api({
     // authorization: 'e0347de3-3f3e-4b66-951f-031aa57f2a82',
     'Content-Type': 'application/json'
   },
-  // credentials: 'include',
+  credentials: 'include',
 })
--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'https://tsverkunov-mesto-b.nomorepartiesxyz.ru'
+export const BASE_URL = 'https://tsverkunov-mesto-b.nomorepartiesxyz.ru';
 
 const checkRequest = (res) => {
   if (res.ok) {
@@ -7,32 +7,33 @@ const checkRequest = (res) => {
   return res.json()
     .then((data) => {
       throw new Error(data.error);
-    })
-}
+    });
+};
 
-export const register = ({email, password}) => {
+export const register = ({ email, password }) => {
   return fetch(`${BASE_URL}/signup`, {
     method: 'POST',
     headers: {
       'Accept': 'application/json',
-      'Content-Type': 'application/json'
+      'Content-Type': 'application/json',
     },
-    body: JSON.stringify({email, password})
+    body: JSON.stringify({ email, password }),
   })
-    .then(checkRequest)
-}
+    .then(checkRequest);
+};
 
-export const authorized = ({email, password}) => {
+export const authorized = ({ email, password }) => {
   return fetch(`${BASE_URL}/signin`, {
     method: 'POST',
     headers: {
       'Accept': 'application/json',
-      'Content-Type': 'application/json'
+      'Content-Type': 'application/json',
     },
-    body: JSON.stringify({email, password})
+    body: JSON.stringify({ email, password }),
+    credentials: 'include',
   })
-    .then(checkRequest)
-}
+    .then(checkRequest);
+};
 
 export const getUserData = (token) => {
   return fetch(`${BASE_URL}/users/me`, {
@@ -40,8 +41,9 @@ export const getUserData = (token) => {
     headers: {
       'Accept': 'application/json',
       'Content-Type': 'application/json',
-      'Authorization': `Bearer ${token}`
-    }
+      // 'Authorization': `Bearer ${token}`,
+    },
+    credentials: 'include',
   })
-    .then(checkRequest)
-}
+    .then(checkRequest);
+};
